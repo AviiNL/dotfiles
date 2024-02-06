25a9b2d7bb5b4ebef26c64e14ab3823a9f7b16d9
AviiNL <git@avii.nl>
2024-02-07 00:25:31 +0100

    open in ... for nemo

--- home-manager/nemo.nix
+++ home-manager/nemo.nix
@@ -1,3 +1,30 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs.cinnamon; [ nemo-with-extensions ];
+
+  xdg.dataFile."nemo/actions/vscode.nemo_action".text = ''
+    [Nemo Action]
+
+    Active=true
+    Name=Open in VS Code
+    Comment=Opens current folder in VS Code
+    Exec=code %P
+    Selection=none
+    Extensions=any;
+    Quote=double
+    Dependencies=code;
+  '';
+
+  xdg.dataFile."nemo/actions/kitty.nemo_action".text = ''
+    [Nemo Action]
+
+    Active=true
+    Name=Open in Kitty
+    Comment=Opens current folder in Kitty
+    Exec=kitty %P
+    Selection=none
+    Extensions=any;
+    Quote=double
+    Dependencies=kitty;
+  '';
+
 }
